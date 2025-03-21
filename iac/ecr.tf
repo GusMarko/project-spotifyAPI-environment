df5f5cca37ecace24f559106223a47e6021e35e2
Marko Gusic <markoggusic@gmail.com>
2025-03-21 11:36:52 +0100

    fix ecr configuratiom

--- iac/ecr.tf
+++ iac/ecr.tf
@@ -14,25 +14,12 @@ data "aws_iam_policy_document" "ecr_policy_document" {
     effect = "Allow"
 
     principals {
-      type        = "AWS"
+      type        = "Service"
       identifiers = ["lambda.amazonaws.com"] 
     }
 
     actions = [
-      "ecr:GetDownloadUrlForLayer",
-      "ecr:BatchGetImage",
-      "ecr:BatchCheckLayerAvailability",
-      "ecr:PutImage",
-      "ecr:InitiateLayerUpload",
-      "ecr:UploadLayerPart",
-      "ecr:CompleteLayerUpload",
-      "ecr:DescribeRepositories",
-      "ecr:GetRepositoryPolicy",
-      "ecr:ListImages",
-      "ecr:DeleteRepository",
-      "ecr:BatchDeleteImage",
-      "ecr:SetRepositoryPolicy",
-      "ecr:DeleteRepositoryPolicy",
+      "ecr:*"
     ]
   }
 }
